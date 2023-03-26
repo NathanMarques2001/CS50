aabc213cb404413a7500f5b3c7a5f9effd17f873
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-03-26 00:43:09 -0300

    Create cash.c

---
+++ C/cash.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int calculateChangeAmount(float value);
+float getValue(void);
+int changeCoin(int value);
+
+int main(void)
+{
+  float change = getValue();
+
+  printf("%i\n", calculateChangeAmount(change));
+}
+
+int calculateChangeAmount(float value)
+{
+  int coinsValue = value * 100;
+  coinsValue %= 100;
+
+  int quantityOfCoins = 0;
+
+  do
+  {
+    int x = changeCoin(coinsValue);
+    coinsValue -= x;
+    quantityOfCoins++;
+  }
+  while(coinsValue > 0);
+
+  return quantityOfCoins;
+}
+
+float getValue(void)
+{
+   float x;
+   printf("Insira o valor do troco: \n");
+
+   do
+   {
+     x = get_float("-> ");
+   }
+   while(x <= 0);
+
+  return x;
+}
+
+int changeCoin(int value)
+{
+  int x;
+  if(value >= 25)
+  {
+    x = 25;
+  }
+  else if(value >= 10)
+  {
+    x = 10;
+  }
+  else if(value >= 5)
+  {
+    x = 5;
+  }
+  else
+  {
+    x = 1;
+  }
+  return x;
+}
